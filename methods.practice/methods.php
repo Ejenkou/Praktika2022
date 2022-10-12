10a59cefcf5f4bb9df0164b5f5f3bb36883fb36d
Ejenkou <ejenkou.k@mail.ru>
2022-10-12 12:34:37 +0300

    Work on bugs

--- methods.practice/methods.php
+++ methods.practice/methods.php
@@ -1,5 +1,5 @@
 <?php
 
-#Задача 1.1
+#Задача 1.5
 
-echo $_FILES['image']['size']."Фильм c названием ".$_POST['fname']. ", который ".$_POST['inthecinema']."показывали в кинотеатрах".$_POST['cinema'].$_POST['Zanr'].$_POST['rating']."Кассовые сборы составили ".$_POST['money']."$ и выше, посмотрело фильм примерно ".$_POST['views']." или более людей. Описание его гласит: '".$_POST['descript']."'";
+echo $_FILES['image']['name'].$_FILES['image']['type']." : "."Фильм c названием ".$_POST['fname']. ", который ".$_POST['inthecinema']."показывали в кинотеатрах".$_POST['cinema'].$_POST['Zanr'].$_POST['rating']."Кассовые сборы составили ".$_POST['money']."$ и выше, посмотрело фильм примерно ".$_POST['views']." или более людей. Описание его гласит: '".$_POST['descript']."'";
