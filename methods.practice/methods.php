81aa955e892e53cec74f90a39382708b544ba26e
Ejenkou <ejenkou.k@mail.ru>
2022-10-12 13:58:13 +0300

    del placeholders and updating php

--- methods.practice/methods.php
+++ methods.practice/methods.php
@@ -2,4 +2,12 @@
 
 #Задача 1.5
 
-echo $_FILES['image']['name'].$_FILES['image']['type']." : "."Фильм c названием ".$_POST['fname']. ", который ".$_POST['inthecinema']."показывали в кинотеатрах".$_POST['cinema'].$_POST['Zanr'].$_POST['rating']."Кассовые сборы составили ".$_POST['money']."$ и выше, посмотрело фильм примерно ".$_POST['views']." или более людей. Описание его гласит: '".$_POST['descript']."'";
+if ($_FILES && $_FILES["image"]["error"]== UPLOAD_ERR_OK)
+{
+    $name = $_FILES["image"]["name"];
+    move_uploaded_file($_FILES["image"]["tmp_name"], $name);
+    echo "Файл загружен:".$_FILES['image']['name'].$_FILES['image']['type'];
+}
+
+
+echo " - "." Фильм под названием ".$_POST['fname']. ", который ".$_POST['inthecinema']."показывали в кинотеатрах".$_POST['cinema'].$_POST['Zanr'].$_POST['rating']."Кассовые сборы составили ".$_POST['money']."$ и выше, посмотрело фильм примерно ".$_POST['views']." или более людей. Описание его гласит: '".$_POST['descript']."'";
